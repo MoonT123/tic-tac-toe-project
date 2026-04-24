6fd3bdcf5cd7e2e1cf006083ee6c0bb2b8fb799c
nguyen ngocminh <nguyenngocminh26052005@gmail.com>
2026-04-25 00:37:12 +0700

    fix player component

--- src/Component/Player.jsx
+++ src/Component/Player.jsx
@@ -1,33 +1,46 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}){
+export default function Player({ name, symbol }) {
   const [editName, setEditName] = useState(name);
-  const[isEditing, setIsEditing] = useState(false);
-  function handleChange(event){
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleChange(event) {
     setEditName(event.target.value);
   }
-  function whenClick(){
+
+  function whenClick() {
     setIsEditing(!isEditing);
   }
-  let playerName = <span className = "player-name ">
-              {editName}
-            </span>;
-  if (isEditing){
-    playerName = <input type = "text" required value= {editName} onChange={handleChange}/>;
-  }
-  
-    return(
-<li>
-  
-            <span className = "player">
-            
-            {playerName}
-            
-            <span className = "player-symbol">
-              {symbol}
-            </span>
-            </span>
-            <button onClick = {whenClick}> {isEditing ? "Save": "Edit"}</button>
-            </li>
+
+  let playerName = (
+    <span className="player-name">
+      {editName}
+    </span>
+  );
+
+  if (isEditing) {
+    playerName = (
+      <input
+        type="text"
+        required
+        value={editName}
+        onChange={handleChange}
+      />
     );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">
+          {symbol}
+        </span>
+      </span>
+
+      <button onClick={whenClick}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
 }
